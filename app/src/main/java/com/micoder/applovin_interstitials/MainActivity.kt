126bf14727c0e86a0c97591c47a8ab16b9291cde
Micoder-dev <micoder.com@gmail.com>
2022-09-06 10:55:24 +0530

    initial commit

---
+++ app/src/main/java/com/micoder/applovin_interstitials/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.micoder.applovin_interstitials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
